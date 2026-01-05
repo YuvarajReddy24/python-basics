dab452e85747b500d36b8fa4288bb24458dd0760
yuvaraj <yuvarajreddy24@gmail.com>
2026-01-05 17:55:54 +0530

    .

--- lists.py
+++ lists.py
@@ -9,9 +9,17 @@ server_1 = servers[0]
 print("server 1 ip address :", server_1)
 
 #Slicing (start_index:end_index + 1:step_size), end_index in python is not inclusive
+#step size: 1 (default)
 simple_slice = servers[1:6:2] # [1, 1+2, 3+2, 5+2]
 print(simple_slice)
 
 
 #Negative_indexing
-simple_slice = servers[-1:-4]
+simple_slice = servers[-1:-4:-1]
+print(simple_slice)
+
+print("Length of list:", len(simple_slice))
+
+#List is a mutable data type
+#Mutable : once define can chnage at any time
+#Immutable : Once define can't be changed
