f1cf9beefd1456bead35f5ada949b1512251497f
yuvaraj <yuvarajreddy24@gmail.com>
2026-01-05 16:51:31 +0530

    .

---
+++ variables.py
@@ -0,0 +1,28 @@
+num_servers = 10    # This is an inline comment
+# This is also comment
+a = 10
+
+# Multi line string
+""" """"""
+This is a
+sample 
+comment
+"""""" """
+
+# Normal string
+"Hello World"
+'Hello World'
+'Today\'s weather is nice'
+"Today's weather is nice"
+
+
+# Datatypes
+"""
+1. Number (Integer,Float)
+2. String
+3. Boolean
+4. List
+5. Tuple
+6. Dict (Dictionaries, Hashmaps)
+7. Sets
+"""
